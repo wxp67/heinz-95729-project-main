6cea216b29d3e657bb2d6c91f11a5e0b1caa4598
Aaratrika-Chakraborty <85408102+Aaratrika-Chakraborty@users.noreply.github.com>
2021-12-01 18:08:12 -0500

    Added the restaurant picker functionality

--- web/scripts/home/homeComponent.js
+++ web/scripts/home/homeComponent.js
@@ -24,9 +24,15 @@ module.exports = {
             <i class="fa fa-caret-down"></i>
             </button>
             <div class="dropdown-content" style="left:0;">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
+              <a href="#">South American/a>
+              <a href="#">Southeast Asian</a>
+              <a href="#">Southern</a>
+              <a href="#">Southwestern</a>
+              <a href="#">Spanish</a>
+              <a href="#">Turkish</a>
+              <a href="#">Thai</a>
+              <a href="#">Vietnamese</a>
+              <a href="#">Ukranian</a>
             </div>
           </div>
           <div class="dropdown" style="float:center;">
@@ -34,29 +40,35 @@ module.exports = {
             <i class="fa fa-caret-down"></i>
             </button>
             <div class="dropdown-content" style="left:0;">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
+              <a href="#">Below $15</a>
+              <a href="#">$15 - $30</a>
+              <a href="#">$30 - $50</a>
+              <a href="#">above $50</a>
             </div>
           </div>
           <div class="dropdown" style="float:center;">
-            <button class="dropbtn" style="margin:5px;">Service Quality Provided
+            <button class="dropbtn" style="margin:5px;">Service Quality
             <i class="fa fa-caret-down"></i>
             </button>
             <div class="dropdown-content" style="left:0;">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
+              <a href="#">Fair Food</a>
+              <a href="#">Extraordinary food</a>
+              <a href="#">Excellent Food</a>
+              <a href="#">Near-perfect Food</a>
+              <a href="#">Fair Service</a>
+              <a href="#">Extraordinary Service</a>
+              <a href="#">Excellent Service</a>
+              <a href="#">Near-perfect Service</a>
             </div>
           </div>
           <div class="dropdown" style="float:center;">
-            <button class="dropbtn" style="margin:5px;">Day of the Week
+            <button class="dropbtn" style="margin:5px;">Visit preference
             <i class="fa fa-caret-down"></i>
             </button>
             <div class="dropdown-content" style="left:0;">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
+              <a href="#">Open on Mondays</a>
+              <a href="#">Open on Sundays</a>
+              <a href="#">Open on weekends</a>
             </div>
           </div>
           <div class="dropdown" style="float:center;">
@@ -64,19 +76,49 @@ module.exports = {
             <i class="fa fa-caret-down"></i>
             </button>
             <div class="dropdown-content" style="left:0;">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
+              <a href="#">Dining Outdoors</a>
+              <a href="#">Dining after Theatre</a>
+              <a href="#">Early Dining</a>
+              <a href="#">Classic Hotel Dining</a>
+              <a href="#">Pre-Theatre Dining</a>
+              <a href="#">Down-Home</a>
+              <a href="#">After Hours Dining</a>
+              <a href="#">Buffet Dining</a>
+              <a href="#">Cafe/Garden Dining</a>
             </div>
           </div>
           <div class="dropdown" style="float:center;">
-            <button class="dropbtn" style="margin:5px;">Amenities provided
+            <button class="dropbtn" style="margin:5px;">Amenities
             <i class="fa fa-caret-down"></i>
             </button>
             <div class="dropdown-content" style="left:0;">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
+              <a href="#">Cab</a>
+              <a href="#">Entertainment</a>
+              <a href="#">Fabulous Views</a>
+              <a href="#">Good Out of Town Business</a>
+              <a href="#">Good for Younger Kids</a>
+              <a href="#">Late Night Menu</a>
+              <a href="#">No Smoking Allowed</a>
+              <a href="#">No Liquour Served</a>
+              <a href="#">Parking and Valet</a>
+              <a href="#">Private rooms available</a>
+              <a href="#">Romantic</a>
+              <a href="#">Single scene</a>
+              <a href="#">Warm space by the fire</a>
+              <a href="#">Wheelchair access</a>
+            </div>
+          </div>
+          <div class="dropdown" style="float:center;">
+            <button class="dropbtn" style="margin:5px;">Fav Restaurants
+            <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content" style="left:0;">
+              <a href="#">Moti Mahal</a>
+              <a href="#">Standard India</a>
+              <a href="#">Bukhara</a>
+              <a href="#">Clay Oven</a>
+              <a href="#">Udipi Palace</a>
+              <a href="#">Ethiopian Village</a>
             </div>
           </div>
         </div>  
